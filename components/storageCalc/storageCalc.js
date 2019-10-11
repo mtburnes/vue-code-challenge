2fecb8add67d5597cf29f430fefac1ad15463500
Michael Burnes <me@mtburnes.dev>
2019-10-11 11:40:25 -0500

    I FORGOT TO COMMIT OFTEN

---
+++ components/storageCalc/storageCalc.js
@@ -0,0 +1,23 @@
+const mult = (x, y) => { return x * y }
+
+const storageCalc = new Vue({
+  el: '#storageCalc',
+  data: {
+    items: 5,
+    size: 3
+  },
+  computed: {
+    storageCalc: function () {
+      const i = parseInt(this.items)
+      const j = parseInt(this.size)
+      const warning = "The calculated value is an absolute value. Negatives are ignored."
+      if(i < 0 || j < 0){
+        return `Your total file size is ${Math.abs(mult(i, j))}kb.\n${warning}`
+      }else{
+        return `Your total file size is ${Math.abs(mult(i, j))}kb.
+      }
+      
+    }
+
+  }
+})
